d094f00cee7e624f262207f4bce4f8d4fd91f939
Kyle Fredrickson <kyfredri@ucsc.edu>
2020-07-19 12:25:12 -0700

    Cleanup.

--- assets/js/datasets.js
+++ assets/js/datasets.js
@@ -91,7 +91,7 @@ window.linqs.datasets.bibtex = function(pub, id) {
     return window.linqs.pubs.bibtex(pub);
 };
 
-window.linqs.datasets.size = function (size) {
+window.linqs.datasets.size = function(size) {
     let sizes = ["B", "K", "M", "G", "T"];
 
     let i = 0;
@@ -100,44 +100,47 @@ window.linqs.datasets.size = function (size) {
         i++;
     }
 
-    return '(' + size.toFixed(1).toString().replace('.0', '') + sizes[i] + ')';
+    return size.toFixed(1).toString().replace('.0', '') + sizes[i];
 }
 
-window.linqs.datasets.makeFullDataset = function(dataset) {
-    let title = dataset['title'];
+window.linqs.datasets.makeDownloadInfo = function(downloads) {
+    let downloadInfosHTML = '';
 
-    let citationKey = dataset['citation-key'];
-
-    let pub = window.linqs.datapubs.pubs[citationKey];
-    let citation = window.linqs.datasets.bibtex(pub, citationKey);
-
-    let description = dataset['description'];
-
-    let downloadInfo = dataset['download-info'];
-    let downloadInfoHTML = '';
-
-    downloadInfo.forEach(function(download, i) {
-        let text = download["text"];
+    downloads.forEach(function(download, i) {
         let link = download['download-link'];
-        let md5 = download['md5'];
-        let size = window.linqs.datasets.size(download['size']);
+        let text = download["text"];
+        let statsHTML = "";
 
-        let downloadTemplate = `
-            <div class='download-full'>
-                <div class='download-link'>
-                    <a href='${link}'>${text} ${size}</a>
+        let sizeRaw = download['size'];
+        let md5 = download['md5'];
+
+        if (sizeRaw != null && md5 != null) {
+            let size = window.linqs.datasets.size(sizeRaw);
+
+            statsHTML = `
+                <div class='size'>
+                    (${size})
                 </div>
                 <div class='hash'>
-                    <p>${md5}</p>
+                    md5: ${md5}
                 </div>
+            `;
+        }
+
+        let downloadInfoHTML = `
+            <div class='download-info'>
+                <a class='download-link' href='${link}'>${text}</a>
+                ${statsHTML}
             </div>
         `;
 
-        downloadInfoHTML += downloadTemplate;
-
+        downloadInfosHTML += downloadInfoHTML
     });
 
-    let references = dataset['references'];
+    return downloadInfosHTML;
+}
+
+window.linqs.datasets.makeReferences = function(references) {
     let referencesHTML = '';
 
     references.forEach(function(reference, i) {
@@ -150,21 +153,72 @@ window.linqs.datasets.makeFullDataset = function(dataset) {
         referencesHTML += referenceTemplate;
     });
 
+    return referencesHTML;
+}
+
+window.linqs.datasets.makeStubDatasets = function() {
+    let stubs = '<p>This page contains datasets used by the LINQS Lab and all exhibit relational structure. If you use them, please cite them accordingly.</p>';
+
+    Object.keys(window.linqs.datasets.datasets).sort().forEach(function(key) {
+
+        let dataset = window.linqs.datasets.datasets[key];
+
+        let title = dataset['title'];
+        let description = dataset['description'];
+        let link = '#' + key;
+
+        let downloads = dataset['download-info'];
+        let downloadInfosHTML = window.linqs.datasets.makeDownloadInfo(downloads);
+
+        stubs += `
+            <hr />
+            <div class='dataset-stub'>
+                <div class='title'>
+                    <h1><a href='${link}'>${title}</a></h1>
+                </div>
+                <div class='short-description'>
+                    ${description}
+                </div>
+                <div class='download-list'>
+                    ${downloadInfosHTML}
+                </div>
+            </div>
+        `;
+    });
+
+    document.querySelector('.datasets').innerHTML = stubs;
+};
+
+window.linqs.datasets.makeFullDataset = function(dataset) {
+    let title = dataset['title'];
+
+    let downloads = dataset['download-info'];
+    let downloadInfosHTML = window.linqs.datasets.makeDownloadInfo(downloads);
+
+    let description = dataset['description'];
+
+    let citationKey = dataset['citation-key'];
+    let pub = window.linqs.datapubs.pubs[citationKey];
+    let citation = window.linqs.datasets.bibtex(pub, citationKey);
+
+    let references = dataset['references'];
+    let referencesHTML = window.linqs.datasets.makeReferences(references)
+
     let templateString = `
         <div class='dataset-full'>
             <div class='top'>
                 <div class='title'>
                     <h1>${title}</h1>
                 </div>
-                <div class='downloads-full'
-                    ${downloadInfoHTML}
+                <div class='download-list'>
+                    ${downloadInfosHTML}
                 </div>
             </div>
             <div class='description'>
                 ${description}
             </div>
             <div class='citation'>
-                <p>If you use this dataset, please use this citation.</p>
+                <p>If you use this dataset, please use the following citation:</p>
                 <pre>${citation}</pre>
             </div>
             <div class='references'>
@@ -179,72 +233,15 @@ window.linqs.datasets.makeFullDataset = function(dataset) {
     document.querySelector('.datasets').innerHTML = templateString;
 };
 
-window.linqs.datasets.makeStubDatasets = function() {
-    let stubs = '<p>These datasets are made available and are often used by the LINQS Lab in our publications. We use them to test new, interesting methods to solve collective prediction problems, and we hope you will find them useful too!</p>';
-
-    Object.keys(window.linqs.datasets.datasets).sort().forEach(function(key) {
-
-        let dataset = window.linqs.datasets.datasets[key];
-
-        let title = dataset['title'];
-        let shortDescription = dataset['description'];
-        let link = '#' + key;
-
-        let downloadInfo = dataset['download-info'];
-        let downloadInfoHTML = '';
-
-        downloadInfo.forEach(function(download, i) {
-            let text = download["text"];
-            let link = download['download-link'];
-            let size = window.linqs.datasets.size(download['size']);
-
-            let md5 = download['md5'];
-            console.log(md5)
-
-            let downloadTemplate = `
-                <div class='download-short'>
-                    <div class='download-link'>
-                        <a href='${link}'>${text} ${size}</a>
-                    </div>
-                    <div class='hash'>
-                        <p>${md5}</p>
-                    </div>
-                </div>
-            `;
-
-            downloadInfoHTML += downloadTemplate;
-
-        });
-
-        stubs += `
-            <div class='dataset-stub'>
-                <div class='title'>
-                    <h1><a href='${link}'>${title}</a></h1>
-                </div>
-                <div class='short-description'>
-                    ${shortDescription}
-                </div>
-                <div class='downloads-short'
-                    ${downloadInfoHTML}
-                </div>
-            </div>
-        `;
-    });
-
-    document.querySelector('.datasets').innerHTML = stubs;
-};
-
 window.linqs.datasets.display = function() {
-
     let datasetID = location.hash.trim().replace(/^#/, '');
 
-    if (datasetID === '')
+    if (datasetID === '') {
         window.linqs.datasets.makeStubDatasets();
-    else {
+    } else {
         if (window.linqs.datasets.datasets.hasOwnProperty(datasetID)) {
             window.linqs.datasets.makeFullDataset(window.linqs.datasets.datasets[datasetID]);
-        }
-        else {
+        } else {
             window.linqs.datasets.makeStubDatasets();
         }
     }
